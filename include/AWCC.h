d99231ecf2de5dfe06f8c1427c854b4fc70a0d08
tr1x_em <admin@trix.is-a.dev>
2025-09-08 14:30:38 +0530

    AWCCRemote_t -> AWCC_t

--- include/AWCC.h
+++ include/AWCC.h
@@ -28,34 +28,36 @@ typedef int AWCCTemperature_t;
 typedef int AWCCFanRpm_t;
 
 struct AWCC_t {
-	void (* Initialize) (void);
+    void (*Initialize)(void);
+    
+    AWCCBoost_t (*GetCpuBoost)(void);
+    AWCCBoost_t (*GetGpuBoost)(void);
+    
+    void (*SetCpuBoost)(AWCCBoost_t);
+    void (*SetGpuBoost)(AWCCBoost_t);
+    
+    AWCCTemperature_t (*GetCpuTemperature)(void);
+    AWCCTemperature_t (*GetGpuTemperature)(void);
+    
+    AWCCFanRpm_t (*GetCpuRpm)(void);
+    AWCCFanRpm_t (*GetGpuRpm)(void);
+    
+    AWCCBoost_t (*GetFanBoost)(enum AWCCFan_t);
+    void (*SetFanBoost)(enum AWCCFan_t, AWCCBoost_t);
+    AWCCTemperature_t (*GetFanTemperature)(enum AWCCFan_t);
+    AWCCFanRpm_t (*GetFanRpm)(enum AWCCFan_t);
+    const char *(*GetFanName)(enum AWCCFan_t);
+    
+    void (*SetMode)(enum AWCCMode_t);
+    enum AWCCMode_t (*GetMode)(void);
+    const char *(*GetModeName)(enum AWCCMode_t);
+    void (*ToggleGMode)(void);
+    
+    enum AWCCPowerState_t (*PowerState)(void);
+    
+    void (*Deinitialize)(void);
+};
 
-	AWCCBoost_t (* GetCpuBoost) (void);
-	AWCCBoost_t (* GetGpuBoost) (void);
-
-	void (* SetCpuBoost) (AWCCBoost_t);
-	void (* SetGpuBoost) (AWCCBoost_t);
-
-	AWCCTemperature_t (* GetCpuTemperature) (void);
-	AWCCTemperature_t (* GetGpuTemperature) (void);
-
-	AWCCFanRpm_t (* GetCpuRpm) (void);
-	AWCCFanRpm_t (* GetGpuRpm) (void);
-
-	AWCCBoost_t (* GetFanBoost) (enum AWCCFan_t);
-	void (* SetFanBoost) (enum AWCCFan_t, AWCCBoost_t);
-	AWCCTemperature_t (* GetFanTemperature) (enum AWCCFan_t);
-	AWCCFanRpm_t (* GetFanRpm) (enum AWCCFan_t);
-	const char * (* GetFanName) (enum AWCCFan_t);
-
-	void (* SetMode) (enum AWCCMode_t);
-	enum AWCCMode_t (* GetMode) (void);
-	const char * (* GetModeName) (enum AWCCMode_t);
-	void (* ToggleGMode) (void);
-
-	enum AWCCPowerState_t (* PowerState) (void);
-
-	void (* Deinitialize) (void);
-} extern const AWCC;
+extern const struct AWCC_t AWCC;
 
 # endif // __AWCC_H
