5cd70f2a9395fff8615c9ab6bde3006847725855
tr1x_em <admin@trix.is-a.dev>
2025-09-08 10:59:51 +0530

    refactor(lights): better way

---
+++ include/lighting_controls.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef enum {
+	LIGHTING_BRIGHTNESS,
+	LIGHTING_STATIC,
+	LIGHTING_SPECTRUM,
+	LIGHTING_BREATHE,
+	LIGHTING_RAINBOW,
+	LIGHTING_WAVE,
+	LIGHTING_BKF,
+	LIGHTING_DEFAULTBLUE,
+	LIGHTING_COMMAND_COUNT
+} lighting_command_id_t;
+
+typedef struct {
+	lighting_command_id_t id;
+	const char *command;
+	const char *description;
+	const char *usage;
+	int min_args;
+	int max_args;
+} lighting_command_t;
+
+int execute_lighting_command(int argc, char **argv);
+void list_lighting_commands(void);
+void generate_lighting_help(void);
