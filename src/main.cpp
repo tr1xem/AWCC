1d16b0fb86dbbd4445e076a6895da689830cdfe3
tr1xem <admin@trix.is-a.dev>
2025-09-22 20:40:39 +0530

    refactor(thermal):cool mode for testing

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
     loguru::init(argc, argv);
 
     Thermals awccthermals;
-    // awccthermals.setThermalMode(ThermalModes::Cool);
+    awccthermals.setThermalMode(ThermalModes::Cool);
     awccthermals.setThermalMode(ThermalModes::Performance);
+    return 0;
 }
