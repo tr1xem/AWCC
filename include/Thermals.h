23e33a98926d5bb0c15778c0107e55629959b2a9
tr1x_em <admin@trix.is-a.dev>
2025-09-22 18:12:52 +0530

    refactor(database): enum class instead of enum

--- include/Thermals.h
+++ include/Thermals.h
@@ -8,44 +8,44 @@ class Thermals {
 
     ThermalModeSet m_mapToBitset(ThermalModes mode) {
         switch (mode) {
-        case Quiet:
-            return b_Quiet;
-        case Balanced:
-            return b_Balanced;
-        case Performance:
-            return b_Performance;
-        case BatterySaver:
-            return b_BatterySaver;
-        case Cool:
-            return b_Cool;
-        case FullSpeed:
-            return b_FullSpeed; // if needed
-        case Gmode:
-            return b_GMode;
-        case Manual:
-            return b_Manual;
+        case ThermalModes::Quiet:
+            return ThermalModeSet::Quiet;
+        case ThermalModes::Balanced:
+            return ThermalModeSet::Balanced;
+        case ThermalModes::Performance:
+            return ThermalModeSet::Performance;
+        case ThermalModes::BatterySaver:
+            return ThermalModeSet::BatterySaver;
+        case ThermalModes::Cool:
+            return ThermalModeSet::Cool;
+        case ThermalModes::FullSpeed:
+            return ThermalModeSet::FullSpeed;
+        case ThermalModes::Gmode:
+            return ThermalModeSet::GMode;
+        case ThermalModes::Manual:
+            return ThermalModeSet::Manual;
         default:
-            return b_Manual; // fallback
+            return ThermalModeSet::Manual; // fallback
         }
     }
 
     const char *thermalModeToString(ThermalModes mode) {
         switch (mode) {
-        case Quiet:
+        case ThermalModes::Quiet:
             return "Quiet";
-        case Balanced:
+        case ThermalModes::Balanced:
             return "Balanced";
-        case Performance:
+        case ThermalModes::Performance:
             return "Performance";
-        case BatterySaver:
+        case ThermalModes::BatterySaver:
             return "Battery Saver";
-        case Cool:
+        case ThermalModes::Cool:
             return "Cool";
-        case Gmode:
+        case ThermalModes::Gmode:
             return "G-Mode";
-        case Manual:
+        case ThermalModes::Manual:
             return "Manual";
-        case FullSpeed:
+        case ThermalModes::FullSpeed:
             return "Full Speed";
         default:
             return "Unknown";
